295e5c5fbe71936d99ad4dd6e95bd80aa3309bda
ProximaOlder <apion149@gmail.com>
2021-01-30 19:28:09 +0200

    add react-cli

---
+++ cli/create.js
@@ -0,0 +1,28 @@
+const fs = require('fs'); 
+const path = require('path'); 
+const minimist = require('minimist'); 
+const args = minimist(process.argv);
+const srcPath = [__dirname, '..', 'src']; 
+const arrPath = args.path.split('/'); 
+const componentName = arrPath[arrPath.length - 1]; 
+const currentArray = [];
+arrPath.forEach(element => {
+  currentArray.push(element);
+  const currentResolvePath = path.resolve(...srcPath, ...currentArray);
+  if (!fs.existsSync(currentResolvePath)) { 
+    fs.mkdirSync(currentResolvePath); 
+  }
+});
+const componentPath = [...srcPath, ...arrPath];
+const componentCode = `import React from 'react';
+import './${componentName}.css';
+const ${componentName} = () => {
+  return (
+    <div className="wrapper">
+    </div>
+  );
+};
+export default ${componentName};`;
+fs.writeFileSync(path.resolve(...componentPath, `${componentName}.jsx`), componentCode);
+const styleCode = '.wrapper {}';
+fs.writeFileSync(path.resolve(...componentPath, `${componentName}.css`), styleCode);
